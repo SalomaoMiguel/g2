0f4c820b803cec2fffd596f3be6e64ea620fda5b
salomaoMiguel <salomaombono@outlook.com>
2021-02-28 15:42:00 -0300

    #Implementacao - implementando configurações(Perfil, Editar perfil, Noticias, convites)

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,10 @@ Rails.application.routes.draw do
     post 'configuracao/profile_edit'
     get 'configuracao/noticia'
     post 'configuracao/noticia'
+    get 'configuracao/convite'
+    post 'configuracao/convite'
+    get 'configuracao/usuario'
+    post 'configuracao/usuario'
     end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
