1b6d53f2ac916e3b567c895320a7ede0190cebd2
salomaoMiguel <salomaombono@outlook.com>
2021-03-01 16:25:56 -0300

    #Mudança - update de webpacker

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,14 +1,3 @@
 const { environment } = require('@rails/webpacker')
 
-//Customs
-const customComfig = require('./customer');
-const webpack = require("webpack")
-
-environment.plugins.append("Provide", new webpack.ProvidePlugin({
-    $: 'jquery/src/jaquery',
-    JQuery: 'jquery/src/jquery',
-    Popper: ['popper.js', 'default'],
-}));
-environment.config.merge(customComfig);
-
 module.exports = environment
